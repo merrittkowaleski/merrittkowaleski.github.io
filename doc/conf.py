5e4fd2247ede26ef2fdca24e912569bda1e1ff92
Merritt Kowaleski <merrittk@cmu.edu>
2025-07-19 15:57:28 -0400

    change theme

--- doc/conf.py
+++ doc/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
-release = '0.1'
+project = 'Merritt Kowaleski\'s personal page'
+copyright = '2025, Merritt Kowaleski'
+author = 'Merritt Kowaleski'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
@@ -21,8 +21,24 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+# Place rst_epilog at the end of every source file
+# line 1: easily write 'ngautonml' with proper formatting
+# line 2: allows inline custom html
+rst_epilog = """
+.. |ngautonml| replace:: ngAuto\\ :sup:`n`\\ ML
+
+.. role:: raw-html(raw)
+   :format: html
+
+"""
+
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
